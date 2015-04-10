90998221cbde28f8252bd6359920652585e522c8
Andrew Cloete <andrewhcloete@gmail.com>
2015-04-10 19:03:17 +0200

    First formal commit. Ever.
    First commit of Geyser simulator.
    Contains the perliminary psuedo code. 
    It only prints one line to std.out: "Baby steps."

---
+++ GeyserSimulator/src/acza/sun/ee/geyserm2m/simulator/GeyserSimulator.java
@@ -0,0 +1,114 @@
+/* --------------------------------------------------------------------------------------------------------
+ * DATE:	10 Apr 2015
+ * AUTHOR:	Cloete A.H
+ * PROJECT:	M-Eng, Inteligent geyser M2M system.	
+ * ---------------------------------------------------------------------------------------------------------
+ * DESCRIPTION: This software simulates geyser instrumentation firmware.
+ * ---------------------------------------------------------------------------------------------------------
+ * PURPOSE: - A stand-alone application that acts and behaves in every way like geyser firmware, running on 
+ * 				instrumentation attached to a real geyser. 
+ * 			- gain insight in firmware design requirements
+ * 			- use as a tool to benchmark the M2M network system
+ * 			- gain insight in geyser behavior (model assessment)
+ * ---------------------------------------------------------------------------------------------------------
+ */
+
+package acza.sun.ee.geyserm2m.simulator;
+
+public class GeyserSimulator {
+	
+	//API Read-only parameters
+	private char critical_flags = 0;
+
+	//API Read-Write parameters
+	private enum Control_mode {CLOSED, OPEN}
+	Control_mode[] control_mode = Control_mode.values();
+	private boolean element_request = false;
+	
+	
+	//Internal parameters
+	private static final int default_setpointHigh = 55;
+	private static final int default_setpointLow = 45;
+	
+	private final int API_TIMEOUT_RESET = 100;
+	private int api_timeout;
+	private boolean api_active = false; 
+	
+	private final int PORT = 3000;
+
+	/*
+	 * Represents the main entry point of firmware.
+	 */
+	public static void main(String [] args)
+	{
+		System.out.println("Baby steps");
+		
+		/*
+		 * Initialize
+			 * Thread Listeners that checks bursts/power failures "Interrupt routine"
+			 * Initialize TCP handlers 
+			 	* that captures data and packs it in the in-bound buffer
+			 	* transmits data present in the out-bound buffer.
+			 * Start api_timeout count down thread "Interrupt timer routine"
+			 * Set Control_mode to default
+			 * Reads persistence file 
+			 * Initialize virtual geyser.
+			 	* Calculates time elapsed between fail-over and restart.
+			 	* Fast-forwards virtual geyser up to current time.
+		 */
+		
+		
+		/*	MAIN PROGRAM LOOP
+		 * 
+			 * Control: switch case(Control_mode)
+				 * CLOSED:
+				 * 		- Switch element according to internal temperature, between default dead-bands.
+				 * OPEN:
+				 * 		- Switch element according to user suggestion 
+			 * 
+			 * Step virtual geyser. 
+			 * 
+			 * 
+			 * API Command server:
+			 	* Check if new command is available in in-bound buffer.
+			 	* Resets ack flag.
+			 	* Evaluates command integrity.
+			 	* Respond accordingly:
+			 		* Set system parameters,
+			 		* or post reply data in out-bound buffer.
+			 		* 
+		 * 
+		 * sleep 1 second.
+		 * LOOP
+		 */
+		
+	}
+	
+	// ---------------------------- THREADS ---------------------------
+	
+	/* 
+	 * API timeout 
+		 * Thread starts when OPEN-LOOP CONTROL message is passed.
+		 * (Every time TCP activity occurs, api_timeout = API_TIMEOUT_RESET.)
+		 * If time runs out --> control_mode = CLOSED and thread dies;
+	 */
+	
+	// --------------------------- LISTENERS --------------------------
+	 
+	/*
+	 * TCP reader and packer
+	 	* Listens for in-bound packets and packs buffer
+	 	* Raises ack flag when new commend is received
+	 	* 
+	 * TCP writer
+	 	* Writes out-bound databuffer to TCP client
+	 */
+	
+}
+
+
+/*
+ * ---------------------------------------------------------------------------------------------------------
+ * NOTES:
+ * ---------------------------------------------------------------------------------------------------------
+ */
---
+++ GeyserSimulator/src/acza/sun/ee/geyserm2m/simulator/VirtualGeyser.java
@@ -0,0 +1,51 @@
+/* --------------------------------------------------------------------------------------------------------
+ * DATE:	10 Apr 2015
+ * AUTHOR:	Cloete A.H
+ * PROJECT:	M-Eng, Inteligent geyser M2M system.	
+ * ---------------------------------------------------------------------------------------------------------
+ * DESCRIPTION: This software contains the theoretical model of a geyser and provides access methods that 
+ * 				returns virtual sensor information.
+ * ---------------------------------------------------------------------------------------------------------
+ * PURPOSE: Emulate hardware sensors and actuators
+ * ---------------------------------------------------------------------------------------------------------
+ */
+
+package acza.sun.ee.geyserm2m.simulator;
+
+public class VirtualGeyser {
+	
+	private int internal_temp;
+	private int outlet_temp;
+	private int inlet_temp;
+	private int ambient_temp;
+	
+	private boolean element_state;
+	private boolean valve_state;
+	
+	
+	public VirtualGeyser(){
+		
+	}
+	
+	public void step(int t_delta){
+		
+	}
+	
+	
+	//---------------  ACCESS METHODS -----------------
+	//(In the real world, these methods equate to reading or writing registers, A/D values, IO states etc.)
+	
+	public boolean getElementState(){
+		return this.element_state;
+	}
+	
+	
+	
+
+}
+
+/*
+ * ---------------------------------------------------------------------------------------------------------
+ * NOTES:
+ * ---------------------------------------------------------------------------------------------------------
+ */
